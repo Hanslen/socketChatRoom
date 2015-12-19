5796ce330320ebadd08581d02075a257b58aee4b
Jiahe Chen <psyjc3@exmail.nottingham.ac.uk>
2015-12-19 20:28:40 +0000

    This version is the one to one version

--- app.js
+++ app.js
@@ -35,19 +35,52 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 });
 
 var io = require('socket.io')(server);
+//add
+var sockets = {};
+var user = new Array();
+user.push("everyone");
+//add ends
+
 io.on('connection', function(client){
-	console.log('Client connected...');
+	client.emit("usersfirst", user);
 	client.on('disconnect', function(){
 		console.log(client.nickname+" disconnected:-(");
+		for(var i = 0; i < user.length; i++){
+			if(user[i] == client.nickname){
+				user.splice(i, 1);
+				break;
+			}
+		}
+		client.broadcast.emit("users", user);
+		client.broadcast.emit("chat message", client.nickname+" has left our chatRoom..:-(");
 	});
 	client.on('join', function(name){
 		client.nickname=name;
+		console.log(client.nickname+" has connected!");
+		//add
+		sockets[name] = client;
+		user.push(name);
+		client.broadcast.emit("users", user);
+		client.emit("users", user);
+		client.broadcast.emit("chat message", client.nickname + " has joined our chatRoom..:-)");
+		client.emit("chat message", client.nickname+" has joined our chatRoom..:-)");
+		//add ends
+
 	});
-	client.on('chat message', function(msg){
+	client.on('chat message', function(msg, to){
 		var nickname = client.nickname;
-		client.broadcast.emit("chat message",nickname+": "+msg);
-		client.emit("chat message", nickname+": "+ msg);
-		console.log(nickname+": "+ msg);
+		// client.broadcast.emit("chat message",nickname+": "+msg);
+		if(to == "everyone"){
+			console.log(nickname+": "+msg+" (This is a public message)");
+			client.broadcast.emit("chat message", nickname+": "+msg+" (This is a public message)");
+			client.emit("chat message", nickname+": "+ msg+" (You send a message to everyone)");
+		}else{
+			console.log(nickname+": "+ msg+" ("+nickname+" send a private message to "+to+")")
+			sockets[to].emit("chat message", nickname+": "+msg+"(This is a private message)");
+			client.emit("chat message", nickname+": "+ msg+" (You send a private message to "+to+")");
+		}
+		client.broadcast.emit("users", user);
+		client.emit("users", user);
 	});
 
 
